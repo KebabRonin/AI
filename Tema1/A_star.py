047215a0d1a552b64483b7aeaa79cb2f217d5640
Brassat Alexandru <alrx.brss@gmail.com>
2023-10-25 15:11:31 +0300

    inca ceva

--- Tema1/A_star.py
+++ Tema1/A_star.py
@@ -1,4 +1,5 @@
 import model
+import math
 
 
 def A_star(initial_state, heuristic):
@@ -9,6 +10,7 @@ def A_star(initial_state, heuristic):
 	# here, a state is (base_state, distance_till_state, heuristic)
 
 	while len(unexplored) > 0:
+		print(f"before:\n	expl:{explored}\n	unex:{unexplored}\n\n")
 		current_state, d, h = unexplored.pop(0)
 
 		if model.is_final(current_state):
@@ -19,14 +21,40 @@ def A_star(initial_state, heuristic):
 			if model.validate(current_state, dir):
 				t = model.transition(current_state, dir)
 
-				like_me = filter(lambda st: st[0] == t[0] and st[1] + st[2] < t[1] + t[2], unexplored)
-				if 0 < len(like_me):
-					unexplored.append((like_me[0][0], d + 1, heuristic(t)))
-					unexplored.remove(like_me[0])
-
-				unexplored.append((t, d + 1, heuristic(t)))
+				like_me_unex = list(filter(lambda st: st[0] == t[0] and st[1] + st[2] < t[1] + t[2], unexplored))
+				like_me_ex = list(filter(lambda st: st[0] == t[0] and st[1] < t[1], explored))
+				if 0 < len(like_me_ex):
+					explored.append((t, d + 1, heuristic(t)))
+					explored.remove(like_me_ex[0])
+				elif 0 < len(like_me_unex):
+					unexplored.append((t, d + 1, heuristic(t)))
+					unexplored.remove(like_me_unex[0])
+				else:
+					unexplored.append((t, d + 1, heuristic(t)))
 
 		explored.append((current_state, d, h))
 
 		unexplored = list(sorted(filter(lambda st: (st[1] + st[2]) < best_score,unexplored),
 						   key = lambda st: (st[1] + heuristic(st[0]))))
+		print(f"after:\n	expl:{explored}\n	unex:{unexplored}\n\n")
+
+	print("gata de cautat")
+
+import state_prints as pr
+def hamming(state):
+	# Hamming distance
+	s = state[0]
+	n = int(math.sqrt(len(s)))
+	score = 0
+	# c = [i for i,x in enumerate(testlist) if x == 1]
+	# pr.pretty_print(state)
+
+	for index,i in enumerate(filter(lambda x: x > 0, s)):
+		if i != index + 1 + score:
+			score += 1
+		# score += abs((index+1)%n - i%n) + abs((index+1)//n - i//n)
+
+	return score
+
+
+# manhattan(([2,3,1,0,4,5,6,7,8], None))
--- Tema1/Tema1.py
+++ Tema1/Tema1.py
@@ -3,16 +3,28 @@ import state_prints as sp
 import time
 
 import IDDFS
+import A_star
 
 
-def test_algo(algo):
+def test_algo(algo, *args):
     dirs = ['UP', 'RIGHT', 'DOWN', 'LEFT']
-    examples = [[2, 5, 3, 1, 0, 6, 4, 7, 8], [2, 7, 5, 0, 8, 4, 3, 1, 6], [8, 6, 7, 2, 5, 4, 0, 3, 1]]
+    examples = [
+        [2, 5, 3,
+         1, 0, 6,
+         4, 7, 8],
+
+        [2, 7, 5,
+         0, 8, 4,
+         3, 1, 6],
+
+        [8, 6, 7,
+         2, 5, 4,
+         0, 3, 1]]
     #               6 steps                       23 steps                      31 steps
     for example in examples:
         state = model.init(example)
         t = time.time()
-        rez = algo(state)
+        rez = algo(state, *args)
         t = time.time() - t
         if rez is None:
             print(state, f"--No solution ({t}s) -->", rez)
@@ -22,4 +34,5 @@ def test_algo(algo):
         print()
 
 
-test_algo(IDDFS.IDDFS)
+#test_algo(IDDFS.IDDFS)
+test_algo(A_star.A_star, A_star.hamming)
