c981c1aea64f57bd7a83b8a4998b99469432321a
BRASSAT Alexandru <92524740+KebabRonin@users.noreply.github.com>
2023-11-16 00:05:23 +0200

    Some changes

--- Tema4/main.py
+++ Tema4/main.py
@@ -12,14 +12,16 @@ To construct the data, seven geometric parameters of wheat kernels were measured
 All of these parameters were real-valued continuous.
 """
 
-import random, copy, numpy as np
+import random, math, numpy as np, matplotlib as mpl
 
 def import_data():
 	data = []
 	with open("seeds_dataset.txt", "r") as fd:
 		for line in fd:
 			attrs = line.split()
-			data.append((np.array([float(attr) for attr in attrs[:-1]]), int(attrs[-1])))
+			sample = np.array([float(attr) for attr in attrs[:-1]])
+			expected_label = np.array([int(t==(int(attrs[-1])-1)) for t in range(3)])
+			data.append((sample, expected_label))
 	return data
 
 
@@ -46,15 +48,21 @@ class Layer:
 			'weights': self.weights,
 		})
 
-	def forward_step(self, input):
-		return self.activation(np.dot(input, self.weights) + self.biases)
+	def forward_step(self, input, expected_label=None):
+		prediction = self.activation(np.dot(input, self.weights) + self.biases)
+		self.last_results = np.copy(prediction)
+		# if expected_label:
+		# 	# Calculez eroarea
+		# 	self.delta = expected_label - prediction
+		return prediction
 
-	def backward_step(self):
-		return
+	def backward_step(self, expected_label):
+		delta = self.last_results * self.derivata(expected_label)
+		return delta
 
 
 class NeuralNetwork:
-	def __init__(self, layers: list[Layer], learning_rate, max_epochs, interpret_label_as_outputs):
+	def __init__(self, layers: list[Layer], learning_rate, max_epochs):
 		for l in range(1, len(layers)):
 			if layers[l].inputs != layers[l-1].neurons:
 				raise Exception("Layers do not connect properly")
@@ -62,7 +70,6 @@ class NeuralNetwork:
 		self.layers = layers
 		self.learning_rate = learning_rate
 		self.max_epochs = max_epochs
-		self.interpret = interpret_label_as_outputs
 
 	def __repr__(self):
 		return str({
@@ -83,7 +90,6 @@ class NeuralNetwork:
 				# label = inp
 
 				label = self.predict(sample)
-				expected_label = self.interpret(expected_label)
 
 				if (label != expected_label).any():
 					# TODO Do this properly
@@ -91,6 +97,8 @@ class NeuralNetwork:
 					error = np.array([(b - a) for a, b in zip(label, expected_label)])
 					self.layers[-1].weights += self.learning_rate * np.atleast_2d(sample).T * error
 					self.layers[-1].biases  += self.learning_rate * error
+					for layer in self.layers[:-1]:
+						layer.backward_step(expected_label)
 					# print(self.layers[-1])
 					# print(label, self.interpret(expected_label), error)
 
@@ -101,7 +109,7 @@ class NeuralNetwork:
 
 		for (sample, expected_label) in test_set:
 			label = self.predict(sample)
-			if (label == self.interpret(expected_label)).all():
+			if (label == expected_label).all():
 				correct += 1
 
 		print("Test accuracy:", correct/n)
@@ -117,19 +125,37 @@ def linear_activation(array):
 	for a in range(len(array)):
 		array[a] = int(array[a] > 0)
 	return array
+def ReLU_activation(array):
+	for a in range(len(array)):
+		array[a] = array[a] if int(array[a] > 0) else 0
+	return array
 
+
+def loss_MSE(label, expected_label):
+	s = 0
+	for l, el in zip(label, expected_label):
+		s += (el - l)**2
+	s /= 2
+	return s
+# def loss_CrossEntropy(label, expected_label):
+# 	# s = 0
+# 	# for l, el in zip(label, expected_label):
+# 	# 	s += l * math.log(1e-15 + el)
+# 	return  
 
 nn = NeuralNetwork(layers=[
-	# Layer(inputs=7, neurons=5, activation=linear_activation, derivata=lambda _: 1),
-	Layer(inputs=7, neurons=3, activation=linear_activation, derivata=lambda _: 1),
-], learning_rate=0.1, max_epochs=5, interpret_label_as_outputs=lambda x: np.array([int(t==(x-1)) for t in range(3)]))
+	Layer(inputs=7, neurons=5, activation=ReLU_activation, derivata=lambda x: 1 if x > 0 else 0),
+	Layer(inputs=5, neurons=3, activation=linear_activation, derivata=lambda _: 1),
+], learning_rate=0.1, max_epochs=5)
 
 # Import Data
 dataset = import_data()
+print("Done importing")
 dataset.sort(key= lambda _: random.random())
 train_set, test_set = split_dataset(dataset, p_train= 0.5)
 
 
-nn.train_online(train_set)
+# nn.train_online(train_set)
+# print(nn)
 nn.test(test_set)
-print(nn)
+print(nn.predict(test_set[0][0])) # prima instanta, doar input-ul
